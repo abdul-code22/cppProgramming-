fb09044fa74f99fc0373beab2d687c0f25318cbf
M Abdul Fazal <mmohd.abdul.fazal@gmail.com>
2022-03-05 00:07:15 +0530

    calculator for two digits(int)

---
+++ eighthCpp.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{   //Simple Calculator
+    int a,b;
+    cout<<"Enter the value of a: ";
+    cin>>a;
+    cout<<"Enter the value of b: ";
+    cin>>b;
+
+    char oper;
+    cout<<"Enter the operation you want to perform: ";
+    cin>>oper;
+    switch (oper)
+    {
+    case '+':
+        cout<< (a+b) <<endl;
+        break;
+    case '-':cout<< (a-b) <<endl;
+    break;
+    case '*':cout<< (a*b) <<endl;
+    break;
+    case '/':cout<< (a/b) <<endl;
+    break;
+
+    default:cout<<"Please enter a valid operation";
+        break;
+    }   
+
+
+
+    return 0;
+}
