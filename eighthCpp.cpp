a680d21b46ac77d7b60d7b6744f40612d808fa67
M Abdul Fazal <mmohd.abdul.fazal@gmail.com>
2022-03-05 13:49:49 +0530

    switch and power funtion

--- eighthCpp.cpp
+++ eighthCpp.cpp
@@ -1,11 +1,25 @@
 #include<iostream>
 #include<bits/stdc++.h>
 using namespace std;
+/*   //power funtion
+int power(int a ,int b)
+{   
+    int ans = 1;
+    for (int i = 0; i < b; i++)
+    {
+        ans=ans*a;
+    }
+    return ans;
+}
 int main()
 
-{   //power funtion
-    
-}
+{   
+   int a,b;
+   cin>>a>>b;
+   int ans = power(a,b);
+   cout<<"Anser is "<<ans;
+
+}*/
 
 /*{   //Simple Calculator
     int a,b;
