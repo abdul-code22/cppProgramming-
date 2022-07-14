6e0c0a60b98c6cdadec2772ff2f165b1913e83b6
M Abdul Fazal <mmohd.abdul.fazal@gmail.com>
2022-07-14 15:38:10 +0530

    Testing git command

--- InsertionSortAnalysis.cpp
+++ InsertionSortAnalysis.cpp
@@ -1,35 +1,44 @@
-#include <stdio.h>
-#include <time.h>
-void insertionSort(int arr[], int n){
-int key, j;
-for (int i = 1; i < n; ++i){
-key = arr[i];
-j = i - 1;
-while(arr[j] > key && j >= 0){
-arr[j+1] = arr[j];
-j--;
+#include<stdio.h> 
+#include<time.h>
+void insertion_sort(int[],int); 
+void Display(int[],int);
+int main()
+{
+    clock_t now,later;
+
+    int Array[] = {4,51,2,3,6,7,8,9,103,34,178,23,56,0,67};
+
+
+    int length_Array = sizeof(Array)/sizeof(Array[0]); printf("Unsorted Array\n\n"); 
+    Display(Array,length_Array);
+    now = clock(); insertion_sort(Array,length_Array);
+    later = clock() - now; printf("Sorted Arrray\n\n"); 
+    Display(Array,length_Array);
+    printf("\nTime Taken to Sort : %0.10f Seconds\n",(float)later/CLOCKS_PER_SEC); 
+    return 0;
 }
-arr[j+1] = key;
+
+void insertion_sort(int Array[],int length)
+{
+    int i,index;
+    long long int key;
+    for(i = 1;i < length;i++)
+    {
+ 
+        key = Array[i]; index = i-1;
+        while(key < Array[index] && index >= 0)
+        {
+            Array[index+1] = Array[index]; index--;
+        }
+    Array[index+1] = key;
+    }
 }
-return;
-}
-int main(void){
-int n;
-printf("Number of elements: ");
-scanf("%d",&n);
-int arr[n];
-printf("Enter the %d elements: \n", n);
-for (int i = 0; i < n; ++i){
-scanf("%d",&arr[i]);
-}
-clock_t begin = clock();
-insertionSort(arr, n);
-clock_t end = clock();
-printf("\nSorted Array: ");
-for (int i = 0; i < n; ++i){
-printf("%d ",arr[i]);
-}
-double runningTime = ((double)(end - begin))/CLOCKS_PER_SEC;
-printf("\nRunning Time: %lf sec", runningTime);
-return 0;
+
+void Display(int Array[],int length)
+{
+    int i =0; for(;i<length;i++)
+    {
+        printf("%d\t",Array[i]);
+    }
+    printf("\n\n");
 }
